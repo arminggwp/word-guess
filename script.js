2f061f1b53e3bfa2f1355269b6c3eee6dd7d8798
arminggwp <arminggwp@gmail.com>
2023-07-24 23:06:43 +0200

    make basic design and input functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+function switchInputFocus() {
+  const allBoxes = document.querySelector('.board').children;
+  for (let i = 0; i < allBoxes.length; i++) {
+    allBoxes[i].addEventListener('keyup', function(e) {
+      if (e.keyCode === 8 || e.keyCode === 37) {
+        this.innerText = '';
+        this.previousElementSibling.focus();
+      } else {
+        this.nextElementSibling.focus();
+      };
+    });
+  };
+};
+
+switchInputFocus();
