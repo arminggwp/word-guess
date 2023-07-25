f517c1cc36e718b36156b2bf83f5eca54aa7a49b
arminggwp <arminggwp@gmail.com>
2023-07-25 22:57:11 +0200

    add check guess function

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function switchInputFocus() {
-  const allBoxes = document.querySelector('.board').children;
+  const allBoxes = document.querySelector('.active').children;
   for (let i = 0; i < allBoxes.length; i++) {
     allBoxes[i].addEventListener('keyup', function(e) {
-      if (e.keyCode === 8 || e.keyCode === 37) {
-        this.innerText = '';
+      if (e.keyCode === 8) {
         this.previousElementSibling.focus();
       } else {
         this.nextElementSibling.focus();
@@ -13,3 +12,14 @@ function switchInputFocus() {
 };
 
 switchInputFocus();
+
+const dictionary = ['chair', 'cloud', 'aphid'];
+
+function checkGuess() {
+  const board = document.querySelector('.active');
+  console.log(board.children[4].value.length);
+  if (board.children[4].value.length === 1) {
+    board.nextElementSibling.classList.add('active');
+    board.classList.remove('active');
+  }
+}
