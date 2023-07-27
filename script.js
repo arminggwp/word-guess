1910865d91d67ca2e712253d51084974feab0200
arminggwp <arminggwp@gmail.com>
2023-07-27 23:28:13 +0200

    make rudimentary guess comparison system

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function switchInputFocus() {
           try {
             this.nextElementSibling.focus();
           } catch (err) {
+            compareGuess(this.parentElement);
             this.parentElement.nextElementSibling.children[0].focus();
           }
         };
@@ -17,6 +18,28 @@ function switchInputFocus() {
   }
 };
 
-const dictionary = ['chair', 'cloud', 'aphid'];
+const dictionary = ['a', 'r', 'm', 'i', 'n'];
 
 switchInputFocus();
+
+function compareGuess(elements) {
+  let guess = [];
+  const allInputs = elements.children;
+  for (let i = 0; i < allInputs.length; i++) {
+    guess.push(allInputs[i]);
+  }
+  for (let j = 0; j < guess.length; j++) {
+    if (dictionary.includes(guess[j].value)) {
+      console.log('it includes ' + guess[j]);
+      guess[j].style.backgroundColor = 'yellow';
+      if (guess[j].value === dictionary[j]) {
+        guess[j].style.backgroundColor = 'green';
+        console.log(guess[j].value + ' is in right position');
+      }
+    } else {
+      console.log('does not include ' + guess[j]);
+      guess[j].style.backgroundColor = 'red';
+    }
+  }
+}
+
